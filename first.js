edda42cec4ad09795b0ae936f53a04f6eb13e51b
dhanashri <dhanashri@angularminds.in>
2024-04-17 11:45:03 +0530

    last commit

--- first.js
+++ first.js
@@ -1,9 +1,11 @@
-name="first file"
+name = "first file";
 
-console.log("second")
+console.log("second");
 
-console.log("third")
+console.log("third");
 
-console.log("four")
+console.log("four");
 console.log("fifth");
 console.log("six");
+
+let arr = [1, 2, 3, 4, [5, 5, [5, 6, 7, [7, [9, 8, 7]]]]];
